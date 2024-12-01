7fe6d5863c3224090eb6ab85231c79849f897c52
EduardoSilva03 <eduaardo.silva03@gmail.com>
2024-12-01 19:12:03 -0300

    refatoracao

--- conn.php
+++ conn.php
@@ -7,6 +7,28 @@ $dbname = "recursoshumanos";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Configurar charset para UTF-8
+$conn->set_charset("utf8");
+
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
+
+// Função para verificar login
+function verificarSessao() {
+    session_start();
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: login.php");
+        exit();
+    }
+}
+
+// Função para logout
+function realizarLogout() {
+    if (isset($_POST['logout'])) {
+        session_destroy();
+        header("Location: login.php");
+        exit();
+    }
+}
+?>
